0f29b9f1b792f6fbcde22ccaaba46ca6555a2721
cassiagabriela <31043167+cassiagabriela@users.noreply.github.com>
2017-08-16 17:56:39 -0300

    Add files via upload

---
+++ questao6.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main() {
+char nome[20], inverso[20];
+int tam, i, p=0;
+
+printf("Digite um nome:");
+gets(nome);
+for(i=strlen(nome)-1; i>=0; i--) {
+
+    inverso[p]=nome[i];
+    p++;
+    inverso[p]='\0'; }
+
+if(strcmp(nome, inverso)==0) {
+    printf("E palindromo");
+}
+else
+{
+    printf("Nao e palindromo"); }
+return 0;
+}
+
